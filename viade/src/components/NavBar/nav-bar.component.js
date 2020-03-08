3e9d580d0b805619efe69ab4ab364536e78825ae
javiponton10 <uo264003@uniovi.es>
2020-03-08 14:10:52 +0100

    Change background and logo

--- viade/src/components/NavBar/nav-bar.component.js
+++ viade/src/components/NavBar/nav-bar.component.js
@@ -58,7 +58,7 @@ const NavBar = (props: Props) => {
       <section className="header-wrap">
         <div className="logo-block">
           <Link to="/welcome">
-            <img src="img/logoViade.png" alt="viade" />
+            <img src="img/logoViade.svg" alt="viade" />
           </Link>
         </div>
 
--- viade/src/components/Utils/GradientBackground/gradient-background.style.js
+++ viade/src/components/Utils/GradientBackground/gradient-background.style.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Gradient = styled.div`
   //background-image: linear-gradient(135deg, #7c4dff 0%, #18a9e6 50%, #01c9ea 100%);
   background: rgb(237,251,63);
-  background: linear-gradient(90deg, rgba(237,251,63,1) 0%, rgba(252,149,70,1) 100%);
+  background: linear-gradient(90deg, #67e3c0ff 0%, #046df0f0 100%);
   background-repeat: no-repeat;
   box-sizing: border-box;
   height: 100%;
