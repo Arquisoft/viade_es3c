96eb4fe24792cd152d259b90bebea41c8c39fc88
Sara <uo258060@uniovi.es>
2020-03-09 19:28:46 +0100

    drawing mark points on map

--- viade/src/containers/Route/map.js
+++ viade/src/containers/Route/map.js
@@ -1,5 +1,6 @@
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
-import React from "react";
+import {Map, GoogleApiWrapper, Marker, Polyline} from 'google-maps-react';
+import React from 'react';
+import update from 'react-addons-update';
 
 const mapStyles = {
     width: '73%',
@@ -8,8 +9,43 @@ const mapStyles = {
 
 export class MapContainer extends React.Component {
 
-    getLocation() {
+    constructor(props) {
+        super(props);
+    }
 
+    state = {
+        markers: [{
+            position: {
+                lat: 25.0112183,
+                lng: 121.52067570000001,
+            },
+            key: "Taiwan",
+            defaultAnimation: 2
+        }],
+        center: {
+            lat: 40.4165000,
+            lng: -3.7025600
+        }
+    };
+
+    clickPoint = (event, map, clickEvent) => {
+        let {markers} = this.state;
+        markers = update(markers, {
+            $push: [
+                {
+                    position: {
+                        lat: clickEvent.latLng.lat(),
+                        lng: clickEvent.latLng.lng(),
+                    },
+                    defaultAnimation: 2,
+                    key: Date.now(),
+                },
+            ],
+        });
+        this.setState({markers});
+    };
+
+    getLocation() {
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition((position) => {
                 this.setState({
@@ -22,6 +58,16 @@ export class MapContainer extends React.Component {
         }
     }
 
+    draw() {
+        let markers = [];
+        for (let i = 0; i < this.state.markers.length; i++) {
+            if (i != 0) {
+                markers.push({lat: this.state.markers[i].position.lat, lng: this.state.markers[i].position.lng})
+            }
+        }
+        return markers;
+    };
+
     render() {
         this.getLocation();
         return (
@@ -29,9 +75,22 @@ export class MapContainer extends React.Component {
                 google={this.props.google}
                 zoom={8}
                 style={mapStyles}
-                initialCenter={{ lat: 47.444, lng: -122.176}}
+                onClick={this.clickPoint}
+                center={this.state.center}
             >
-            <Marker position={{ lat: 48.00, lng: -122.00}} />
+
+            {this.state.markers.map((marker) => {
+                return (
+                    <Marker position={{lat: marker.position.lat, lng: marker.position.lng}}/>
+                );
+            })}
+
+            <Polyline
+                path={this.draw()}
+                strokeColor="#0000FF"
+                strokeOpacity={0.8}
+                strokeWeight={2}/>
+
             </Map>
         );
     }
