3354ec96339423605a1156172969d020a0663120
Sara <uo258060@uniovi.es>
2020-03-14 20:01:55 +0100

    Save new routes'data to keep at pod

--- viade/src/containers/NewRoute/NewRoute.js
+++ viade/src/containers/NewRoute/NewRoute.js
@@ -1,52 +1,67 @@
+/* eslint-disable constructor-super */
+/* eslint-disable no-nested-ternary */
+/* eslint-disable no-console */
+
 import React from 'react';
-import {successToaster, errorToaster} from '@utils';
-import {Loader} from '@util-components';
+import {Input} from "../TextEditor/text-editor.style";
 import Map from './Map';
 import {
     Header,
     RouteWrapper,
     Button
 } from './route.style';
-import {Input} from "../TextEditor/text-editor.style";
 
-function saveRoute(name) {
-    let routeName = name;
-    console.log(name)
-}
+type Props = { webId: String };
 
 class NewRoute extends React.Component{
 
-    constructor(props) {
-        super(props);
-        this.state = {value: ''};
-
+    constructor({ webId }: Props) {
+        super();
+        this.webID = webId.replace("profile/card#me","");
+        console.log(this.webID);
+        this.handleSave = this.handleSave.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleChange(event) {
-        this.setState({value: event.target.value});
-    }
-
-    handleSubmit(event) {
-        event.preventDefault();
-        saveRoute(this.state.value)
+        this.title = React.createRef();
     }
 
     state = {markers: {}};
 
     callBackFunction = (childData) => {
         this.setState({markers: childData})
-        console.log(this.state.markers);
     };
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        this.handleSave(event);
+    }
+
+    handleSave(event){
+        if(this.title.current.value.length === 0){
+            alert("La ruta tiene que tener un titulo.")
+        }else if(this.state.markers === 0){
+            alert("No ha marcado ningún punto en el mapa.")
+        }else{
+            //let route = new Route(this.title.current.value,"Ruta",this.state.markers,this.webID,null,  null,  null);
+            //let parser = new RouteToRdfParser(route, this.webID);
+            //parser.parse();
+            alert("Se ha guardado correctamente");
+        }
+        event.preventDefault();
+    }
 
     render(): React.ReactNode {
         return (
             <RouteWrapper data-testid="route-component">
                 <Header>
                     <h5 className="text--white">Nombre:</h5>
-                    <Input type="text" value={this.state.value} onChange={this.handleChange} />
-                    <Button id="saveRoute" type="button" onClick={this.handleSubmit} className="btn">Save</Button>
+                    <Input type="text" placeholder="Nueva ruta" ref={this.title}/>
+                    <Button onClick={this.handleSubmit}> Guardar</Button>
                 </Header>
                 <Map parentCallBack = {this.callBackFunction} zoom={13}/>
             </RouteWrapper>
