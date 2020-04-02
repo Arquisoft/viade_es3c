26a88ed64015e2421e12994d718e1ea571ffef3c
javiponton10 <uo264003@uniovi.es>
2020-04-02 18:55:33 +0200

    change some things

--- src/containers/Welcome/welcome.component.js
+++ src/containers/Welcome/welcome.component.js
@@ -17,7 +17,7 @@ import { errorToaster } from '@utils';
 export const WelcomePageContent = props => {
     const { webId, image, updatePhoto, name } = props;
     const { t } = useTranslation();
-    const limit = 2100000;
+    const limit = 2100000
     return (
         <WelcomeWrapper data-testid="welcome-wrapper">
             <WelcomeCard className="card">
