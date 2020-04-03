270c850e4dcdc630b5093347c751a1d16b37e942
Sara <uo258060@uniovi.es>
2020-04-03 18:12:01 +0200

    index test

--- src/index.js
+++ src/index.js
@@ -5,21 +5,23 @@ import App from './App';
 import { ErrorBoundary, GlobalError } from './components';
 import * as serviceWorker from './serviceWorker';
 
+const renderToDOM = () => {
+  ReactDOM.render(
+    <ErrorBoundary component={(error, info) => <GlobalError error={error} info={info} />}>
+      <App />
+    </ErrorBoundary>,
+    document.getElementById('root')
+  );
+}
 
-
-ReactDOM.render(
-  <ErrorBoundary component={(error, info) => <GlobalError error={error} info={info} />}>
-    <App />
-  </ErrorBoundary>,
-  document.getElementById('root')
-);
+renderToDOM();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
 
-
+export {renderToDOM};
 
 
 
