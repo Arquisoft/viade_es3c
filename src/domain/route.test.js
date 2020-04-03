343ceb1790bf6fc868f68097253284b5a3719f53
Sara <uo258060@uniovi.es>
2020-04-03 18:38:51 +0200

    route test

---
+++ src/domain/route.test.js
@@ -0,0 +1,19 @@
+import { cleanup } from 'react-testing-library';
+import Route from "./route";
+
+afterAll(cleanup);
+
+describe('Route class domain', () => {
+  it('create class route without crashing', () => {
+    const markers = [
+      {position :{lat: 43.354831, lng: -5.851303}},
+      {position :{lat: 43.356440, lng: -5.854693}},
+      {position :{lat: 43.361836, lng: -5.850547}}
+    ];
+    const route = new Route("Prueba", "Sara", "prueba", markers);
+    expect(route.name === "Prueba");
+    expect(route.author === "Sara");
+    expect(route.description === "prueba");
+    expect(route.markers === markers);
+  });
+});
