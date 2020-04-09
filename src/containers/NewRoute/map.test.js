7ffc2fbeaf617f3e97e1ba1eab33575099c0052e
Sara <uo258060@uniovi.es>
2020-04-09 18:24:44 +0200

    update map test

--- src/containers/NewRoute/map.test.js
+++ src/containers/NewRoute/map.test.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { render, cleanup } from 'react-testing-library';
 import { HashRouter as Router } from 'react-router-dom';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
 import Map from "./Map";
 
-library.add(fas);
+const markers = [
+  {position :{lat: 43.354831, lng: -5.851303}},
+  {position :{lat: 43.356440, lng: -5.854693}},
+  {position :{lat: 43.361836, lng: -5.850547}}
+];
 
 describe.only('Map', () => {
   afterAll(cleanup);
   const { container } = render(
     <Router>
-      <Map/>
+      <Map zoom={15} markers={markers}/>
     </Router>
   );
 
