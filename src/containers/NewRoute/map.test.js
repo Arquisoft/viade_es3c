dd42c8b4c95e8bf5f5cd743cdb250defacb79b0c
Sara <uo258060@uniovi.es>
2020-04-09 20:46:42 +0200

    map

--- src/containers/NewRoute/map.test.js
+++ src/containers/NewRoute/map.test.js
@@ -3,17 +3,11 @@ import { render, cleanup } from 'react-testing-library';
 import { HashRouter as Router } from 'react-router-dom';
 import Map from "./Map";
 
-const markers = [
-  {position :{lat: 43.354831, lng: -5.851303}},
-  {position :{lat: 43.356440, lng: -5.854693}},
-  {position :{lat: 43.361836, lng: -5.850547}}
-];
-
 describe.only('Map', () => {
   afterAll(cleanup);
   const { container } = render(
     <Router>
-      <Map zoom={15} markers={markers}/>
+      <Map/>
     </Router>
   );
 
