04ed5176a2e023b65227f9ce3dd0b0b3abfaece4
Gema Rico Pozas <36774778+gemarico@users.noreply.github.com>
2020-04-11 17:55:30 +0200

    fix conflicts

--- src/containers/MyFriends/MyFriends.js
+++ src/containers/MyFriends/MyFriends.js
@@ -1,116 +1,71 @@
-import React from 'react';
+import React from "react";
 import {
-    Header,
-    RouteWrapper,
-    MyRouteContainer,
-    FormRenderContainer,
-    Friends, 
-    FormAddFriends
-} from './myfriends.style';
-import {List} from '@solid/react';
+	Header,
+	RouteWrapper,
+	MyRouteContainer,
+	FormRenderContainer,
+	Friends,
+	FormAddFriends
+} from "./myfriends.style";
+import { List } from "@solid/react";
 import InfoFriends from "./InfoFriends";
 import friendsHelper from "./FriendsHelper";
+import { successToaster } from "@utils";
 
 type Props = { webId: String };
 
-class MyFriends extends React.Component{
-    constructor({ webId }: Props) {
-        super();
-        this.webID=webId;
-        this.state = {
-            friendWebID: ''
-        };
-        this.handleChange = this.handleChange.bind(this);
-    }
+class MyFriends extends React.Component {
+	constructor({ webId }: Props) {
+		super();
+		this.webID = webId;
+		this.state = {
+			friendWebID: ""
+		};
+		this.handleChange = this.handleChange.bind(this);
+	}
 
-    handleChange(event) {
-        this.setState({friendWebID: event.target.value});
-      }
+	handleChange(event) {
+		this.setState({ friendWebID: event.target.value });
+	}
 
-    getList() {
-        return <Friends>
-        <List src={"user.friends"}>{
-            (item, i) =>
-                <InfoFriends
-                    key={i}
-                    name={getUserName(`${item}`)}
-                    url={<a href={getUrl(`${item}`)}>{getUrl(`${item}`)}</a>}
-                    webidUser = {this.webID}
-                    webidFriend = {`${item}`}
-                />
-        }
-        </List>
-        </Friends>;
-    }
+	getList() {
+		return (
+			<Friends>
+				<List src={"user.friends"}>
+					{(item, i) => (
+						<InfoFriends
+							key={i}
+							name={getUserName(`${item}`)}
+							url={<a href={getUrl(`${item}`)}>{getUrl(`${item}`)}</a>}
+							webidUser={this.webID}
+							webidFriend={`${item}`}
+						/>
+					)}
+				</List>
+			</Friends>
+		);
+	}
 
-    
+	handleClick(e) {
+		e.preventDefault();
+		friendsHelper.addFriend(this.webID, this.state.friendWebID);
+		successToaster("Amigo añadido correctamente", "Éxito");
+		refreshPage();
+	}
 
-    
-    handleClick(e){
-        e.preventDefault();
-        friendsHelper.addFriend(this.webID, this.state.friendWebID);
-        console.log("Añadiendo: " + this.webID + " " + this.state.friendWebID);
-        refreshPage();
-
-    }
-
-
-    addFriends(){
-        return (
-            <FormAddFriends>
-            <form>
-                <label>
-                    WebId:
-                    <input type="text" name="webID" onChange={this.handleChange}/>
-                </label>
-                <input type="submit" value="Add" onClick={(e) => this.handleClick(e)}/>
-            </form>
-            </FormAddFriends>
-        );
-    }
-
- render(): React.ReactNode {
-        return (
-             <RouteWrapper data-testid="route-component">
-                 <MyRouteContainer>
-
-                     <FormRenderContainer>
-                             <Header>
-                                 <h1>Adding new Friends</h1>
-                             </Header>
-                             {this.addFriends()}
-                            <Header>
-                                 <h1>My friends</h1>
-                             </Header>
-                             {this.getList()}
-                     </FormRenderContainer>
-                 </MyRouteContainer>
-             </RouteWrapper>
-         );
-     }
-
-
-};
-
-export const getUserName = (name) => {
-    let username = name.replace("https://", "");
-    username = username.username.replace(".inrupt.net/", "").replace("profile/card#me", "");
-    return username.replace(".solid.community/", "").replace("profile/card#me", "");
-}
-
-export const getUrl = (name) => {
-    return name.replace("profile/card#me", "");
-};
-
-export const refreshPage = () => { 
-    window.location.reload(); 
-}
-
-export const removeFriend = (webIdUser,friendWebID) => {
-    friendsHelper.deleteFriend(webIdUser, friendWebID);
-    //refreshPage();
-}
-
+	addFriends() {
+		return (
+			<FormAddFriends>
+				<form>
+					<label>
+						WebId:
+						<input type="text" name="webID" onChange={this.handleChange} />
+					</label>
+					<input type="submit" value="Add" onClick={(e) => this.handleClick(e)} />
+				</form>
+			</FormAddFriends>
+		);
+	}
 
 	render(): React.ReactNode {
 		return (
@@ -118,22 +73,39 @@ export const removeFriend = (webIdUser,friendWebID) => {
 				<MyRouteContainer>
 					<FormRenderContainer>
 						<Header>
+							<h1>Adding new Friends</h1>
+						</Header>
+						{this.addFriends()}
+						<Header>
 							<h1>My friends</h1>
 						</Header>
-						<List src={"user.friends"}>
-							{(item, i) => (
-								<InfoFriends
-									key={i}
-									name={this.getUserName(`${item}`)}
-									url={<a href={this.getUrl(`${item}`)}>{this.getUrl(`${item}`)}</a>}
-								/>
-							)}
-						</List>
+						{this.getList()}
 					</FormRenderContainer>
 				</MyRouteContainer>
 			</RouteWrapper>
 		);
 	}
 }
+
+export const getUserName = (name) => {
+	let author = name.replace("https://", "");
+	author = author.replace(".solid.community/profile/card#me", "");
+	author = author.replace(".inrupt.net/profile/card#me", "");
+	return author;
+};
+
+export const getUrl = (name) => {
+	return name.replace("profile/card#me", "");
+};
+
+export const refreshPage = () => {
+	window.location.reload();
+};
+
+export const removeFriend = (webIdUser, friendWebID) => {
+	friendsHelper.deleteFriend(webIdUser, friendWebID);
+	successToaster("Amigo eliminado correctamente", "Éxito");
+	//refreshPage();
+};
 
 export default MyFriends;
