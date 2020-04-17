f249db6db32a8460f85b4e4ba403fda5a23d2506
Sara <uo258060@uniovi.es>
2020-04-17 20:27:10 +0200

    autosavespinner test

--- src/containers/NewRoute/NewRoute.js
+++ src/containers/NewRoute/NewRoute.js
@@ -14,6 +14,7 @@ import {
 import { Route, Point, Multimedia } from "domain";
 import { MultimediaComponent } from "../UploadMultimedia/multimedia.container";
 import i18n from "i18n";
+import * as viadeManager from "../../utils/storage";
 
 type Props = {
     webId: String,
