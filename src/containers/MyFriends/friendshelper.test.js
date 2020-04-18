9ca82b3be5aacd01af7d00224df6ab933bf96d94
Sara <uo258060@uniovi.es>
2020-04-18 02:13:17 +0200

    friends

---
+++ src/containers/MyFriends/friendshelper.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {cleanup, render} from 'react-testing-library';
+import {HashRouter as Router} from 'react-router-dom';
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {fas} from '@fortawesome/free-solid-svg-icons';
+import InfoFriends from "./InfoFriends";
+
+library.add(fas);
+
+const props = {
+  name: 'elmer',
+  url: ' https://elmer.solid.community/'
+};
+
+describe.only('MyFriends', () => {
+  afterAll(cleanup);
+  const {container} = render(
+    <Router>
+      <InfoFriends {...{...props}} />
+    </Router>
+  );
+
+  test('renders without crashing', () => {
+    expect(container).toBeTruthy();
+  });
+});
