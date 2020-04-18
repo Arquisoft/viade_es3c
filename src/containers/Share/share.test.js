aea803f4306a06b85ce39913d02c9bcfd4aa10ef
Sara <uo258060@uniovi.es>
2020-04-18 02:47:51 +0200

    share test

---
+++ src/containers/Share/share.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { render, cleanup } from 'react-testing-library';
+import { HashRouter as Router } from 'react-router-dom';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { FriendsList } from "../MyFriends/myfriends.style";
+
+library.add(fas);
+
+describe.only("NewRoute", () => {
+  afterAll(cleanup);
+  const { container } = render(
+    <Router>
+      <FriendsList></FriendsList>
+    </Router>
+  );
+
+  test('renders without crashing', () => {
+    expect(container).toBeTruthy();
+  });
+
+});
