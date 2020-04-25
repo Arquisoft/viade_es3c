130dd7f6a6b2e541b05138ac44de59c51872b86a
Gema Rico Pozas <36774778+gemarico@users.noreply.github.com>
2020-04-25 22:09:53 +0200

    fixed pdf dowload

--- src/components/PrintButton/PrintButton.js
+++ src/components/PrintButton/PrintButton.js
@@ -2,19 +2,9 @@ import React from "react";
 import html2canvas from "html2canvas";
 import jsPDF from "jspdf";
 
-const pxToMm = (px) => {
-	return Math.floor(px / document.getElementById("myMm").offsetHeight);
-};
-
-const mmToPx = (mm) => {
-	return document.getElementById("myMm").offsetHeight * mm;
-};
-
-const range = (start, end) => {
-	return Array(end - start).join(0).split(0).map(function(val, id) {
-		return id + start;
-	});
-};
+const auth = require("solid-auth-client");
+const FC = require("solid-file-client");
+const fc = new FC(auth);
 
 const PrintButton = ({ id, label, route }) => (
 	<div className="tc mb4 mt2">
@@ -29,29 +19,35 @@ const PrintButton = ({ id, label, route }) => (
 				html2canvas(input, {
 					backgroundColor: null,
 					useCORS: true,
-					allowTaint: true,
+					allowTaint: false,
 					scrollY: -window.scrollY
 				}).then((canvas) => {
 					var imgData = canvas.toDataURL("image/png", 1.0);
-
 					pdf = new jsPDF();
-
 					pdf.addImage(imgData, "PNG", 10, 10, 190, 190);
+					if (route.multimedia.length > 0) {
+						pdf.addPage();
+						const promises = [];
+						for (var i = 0; i < route.multimedia.length; i++) {
+							var genImg = new Image();
+							genImg.setAttribute("crossOrigin", "anonymous");
+							var r = route.multimedia[i];
+							promises.push(
+								route.multimedia[i].data.then((blob) => {
+									genImg.src = URL.createObjectURL(blob);
+									pdf.addImage(genImg, "PNG", 10, 10, 190, 180);
+									pdf.setFont("courier");
+									pdf.setFontType("normal");
+									pdf.setFontSize(10);
+									pdf.text(10, 200, `by: ${r.author}`);
+									pdf.text(10, 210, `at: ${r.date}`);
+									if (pdf.getNumberOfPages() < route.multimedia.length + 1) pdf.addPage();
+								})
+							);
+						}
 
-					//if (route.multimedia.length > 0) {
-					//	for (var i = 0; i < route.multimedia.length; i++) {
-					//		pdf.addPage();
-					//		var genImg = new Image();
-					//		genImg.setAttribute("crossOrigin", "anonymous");
-					//		genImg.src = route.multimedia[i].url;
-
-					//		pdf.addImage(genImg, "PNG", 10, 10, 190, 150);
-					//		pdf.text(10, 170, `${route.multimedia[i].name}`);
-					//		pdf.text(10, 180, `${route.multimedia[i].author}`);
-					//		pdf.text(10, 190, `${route.multimedia[i].date}`);
-					//	}
-					//	}
-					pdf.save(`${route.name}.pdf`);
+						Promise.all(promises).then((e) => pdf.save(`${route.name}.pdf`));
+					}
 				});
 			}}
 		>
