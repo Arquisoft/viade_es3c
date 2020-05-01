7c74a2b0ef5a55796cf68357c9da03c6a3c3e902
Tania Mato Martínez <taniamato@outlook.es>
2020-05-01 17:35:40 +0200

    formatting code

--- src/routes.js
+++ src/routes.js
@@ -3,14 +3,14 @@ import { PrivateLayout, PublicLayout, NotLoggedInLayout } from "@layouts";
 import { HashRouter as Router, Switch, Redirect } from "react-router-dom";
 
 import {
-	Login,
-	Register,
-	PageNotFound,
-	Welcome,
-	RegistrationSuccess,
-	Profile,
-	FormModelConverter,
-	FormModelRenderer
+  Login,
+  Register,
+  PageNotFound,
+  Welcome,
+  RegistrationSuccess,
+  Profile,
+  FormModelConverter,
+  FormModelRenderer
 } from "./containers";
 import NewRoute from "./containers/NewRoute";
 import MyRoutes from "./containers/MyRoutes";
@@ -18,60 +18,64 @@ import MyFriends from "./containers/MyFriends";
 import Footer from "./components/Footer";
 
 const privateRoutes = [
-	{
-		id: "welcome",
-		path: "/welcome",
-		component: Welcome
-	},
-	{
-		id: "route",
-		path: "/route",
-		component: NewRoute
-	},
-	{
-		id: "myRoutes",
-		path: "/myRoutes",
-		component: MyRoutes
-	},
+  {
+    id: "welcome",
+    path: "/welcome",
+    component: Welcome
+  },
+  {
+    id: "route",
+    path: "/route",
+    component: NewRoute
+  },
+  {
+    id: "myRoutes",
+    path: "/myRoutes",
+    component: MyRoutes
+  },
 
-	{
-		id: "myFriends",
-		path: "/myFriends",
-		component: MyFriends
-	},
+  {
+    id: "myFriends",
+    path: "/myFriends",
+    component: MyFriends
+  },
 
-	{
-		id: "profile",
-		path: "/profile",
-		component: Profile
-	},
-	{
-		id: "formmodelconverter",
-		path: "/formmodel/converter",
-		component: FormModelConverter
-	},
-	{
-		id: "formmodelrenderer",
-		path: "/formmodel/renderer",
-		component: FormModelRenderer
-	}
+  {
+    id: "profile",
+    path: "/profile",
+    component: Profile
+  },
+  {
+    id: "formmodelconverter",
+    path: "/formmodel/converter",
+    component: FormModelConverter
+  },
+  {
+    id: "formmodelrenderer",
+    path: "/formmodel/renderer",
+    component: FormModelRenderer
+  }
 ];
 
 const Routes = () => (
-	<Router>
-		<Fragment>
-			<Switch>
-				<NotLoggedInLayout component={Login} path="/login" exact />
-				<NotLoggedInLayout component={Register} path="/register" exact />
-				<NotLoggedInLayout path="/register/success" component={RegistrationSuccess} exact />
-				<PublicLayout path="/404" component={PageNotFound} exact />
-				<Redirect from="/" to="/welcome" exact />
-				<PrivateLayout path="/" routes={privateRoutes} />
-				<Redirect to="/404" />
-			</Switch>
-			<Footer />
-		</Fragment>
-	</Router>
+  <Router>
+    <Fragment>
+      <Switch>
+        <NotLoggedInLayout component={Login} path="/login" exact />
+        <NotLoggedInLayout component={Register} path="/register" exact />
+        <NotLoggedInLayout
+          path="/register/success"
+          component={RegistrationSuccess}
+          exact
+        />
+        <PublicLayout path="/404" component={PageNotFound} exact />
+        <Redirect from="/" to="/welcome" exact />
+        <PrivateLayout path="/" routes={privateRoutes} />
+        <Redirect to="/404" />
+      </Switch>
+      <Footer />
+    </Fragment>
+  </Router>
 );
 
 export default Routes;
--- src/utils/ldflex.test.js
+++ src/utils/ldflex.test.js
@@ -1,40 +1,50 @@
 import * as ldflex from "./ldflex-helper";
 import { cleanup } from "react-testing-library";
-import React from 'react';
+import React from "react";
 import * as Toaster from "./toaster";
 
 afterAll(cleanup);
 
 describe.only("Ldflex", () => {
-
-  it('renders without crashing', () => {
+  it("renders without crashing", () => {
     ldflex.documentExists("prueba");
     ldflex.createDocument("prueba", "body");
   });
 
-  test('the fetch fails with an error', () => {
-    return ldflex.createDoc("prueba", null).catch(e => expect(e).toMatch('error'));
-    return ldflex.deleteFile("prueba", null).catch(e => expect(e).toMatch('error'));
-    return ldflex.createDocument("prueba", null).catch(e => expect(e).toMatch('error'));
-    return ldflex.createDocumentWithTurtle("prueba", null).catch(e => expect(e).toMatch('error'));
-    return ldflex.getLinkedInbox("prueba").catch(e => expect(e).toMatch('error'));
-    return ldflex.resourceExists("prueba").catch(e => expect(e).toMatch( Toaster.errorToaster(e.message, "Error")));
-
+  test("the fetch fails with an error", () => {
+    return ldflex
+      .createDoc("prueba", null)
+      .catch(e => expect(e).toMatch("error"));
+    return ldflex
+      .deleteFile("prueba", null)
+      .catch(e => expect(e).toMatch("error"));
+    return ldflex
+      .createDocument("prueba", null)
+      .catch(e => expect(e).toMatch("error"));
+    return ldflex
+      .createDocumentWithTurtle("prueba", null)
+      .catch(e => expect(e).toMatch("error"));
+    return ldflex
+      .getLinkedInbox("prueba")
+      .catch(e => expect(e).toMatch("error"));
+    return ldflex
+      .resourceExists("prueba")
+      .catch(e => expect(e).toMatch(Toaster.errorToaster(e.message, "Error")));
   });
 
-  test('documentExists is ok', () => {
+  test("documentExists is ok", () => {
     return ldflex.documentExists("prueba").then(data => {
-      expect(data).toStrictEqual({"ok":true, "status": 200});
+      expect(data).toStrictEqual({ ok: true, status: 200 });
     });
   });
 
-  test('createDocumentTurtle is ok', () => {
+  test("createDocumentTurtle is ok", () => {
     return ldflex.createDocumentWithTurtle("prueba", null).then(data => {
-      expect(data).toStrictEqual({"ok":true, "status": 200});
+      expect(data).toStrictEqual({ ok: true, status: 200 });
     });
-  })
+  });
 
-  test('calls', () => {
+  test("calls", () => {
     expect(ldflex.documentExists("prueba")).toBeTruthy;
     expect(ldflex.createDoc("prueba", null)).toBeTruthy;
     expect(ldflex.deleteFile("prueba")).toBeTruthy;
@@ -45,7 +55,5 @@ describe.only("Ldflex", () => {
     expect(ldflex.resourceExists("prueba")).toBeTruthy;
     expect(ldflex.discoverInbox("prueba")).toBeTruthy;
     expect(ldflex.getLinkedInbox("prueba")).toBeTruthy;
-  })
-
-
+  });
 });
