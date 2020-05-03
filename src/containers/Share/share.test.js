95c5076c5282e1678314e92b7f921f330d4b3007
Gema Rico Pozas <36774778+gemarico@users.noreply.github.com>
2020-05-03 15:51:40 +0200

    Revert "more clean"
    This reverts commit ee67bffa8e6bce30d851600033cf655022ee905a.

---
+++ src/containers/Share/share.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { cleanup } from "react-testing-library";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import Share from "./Share";
+
+library.add(fas);
+
+const Props = {
+	webId: "https://saragarcia.solid.community/"
+};
+
+describe.only("Share", () => {
+	afterAll(cleanup);
+
+	test("class share", () => {
+		const share = new Share(Props);
+		share.permit("friend", "prueba", "sara");
+	});
+});
